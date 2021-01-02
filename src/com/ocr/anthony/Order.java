fc4e3a47c7bccfb0744def4d91c036e03a06723c
Lamia Moshrif <lamiamoshrif@gmail.com>
2021-01-02 12:36:52 +0100

    Add behavior in order : - method displayAvailableMenu - method displaySelectedMenu

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -5,13 +5,16 @@ public class Order {
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
-
+        System.out.println("Choix Menu ");
+        System.out.println("1- Poulet");
+        System.out.println("2- Boeuf");
+        System.out.println("3- Vegetarien");
     }
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-
+        System.out.println("Vous avez choisi le menu " + nbMenu);
     }
 }
